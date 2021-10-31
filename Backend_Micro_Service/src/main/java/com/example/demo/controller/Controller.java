27c03f15a7573b3bc23bbce1795f3be72b9d8a73
Rajat <rajatjain.jain29@gmail.com>
2021-10-31 15:58:14 +0530

    POST and GET APIs for user and order

--- Backend_Micro_Service/src/main/java/com/example/demo/controller/Controller.java
+++ Backend_Micro_Service/src/main/java/com/example/demo/controller/Controller.java
@@ -1,12 +1,8 @@
 package com.example.demo.controller;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,25 +17,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
-	Statement statement;
-	String url = "jdbc:mysql://localhost:3306/ABC_Delivery";
-	String username = "root";
-	String password = "";
+	HashMap<String, String> user = new HashMap<>();
+	HashMap<String, String> order = new HashMap<>();
 
 	@PostMapping("/newUser")
 	public String post(HttpServletRequest request, HttpServletResponse res) throws SQLException {
+
+		user = new HashMap<>();
 
 		String name = request.getParameter("name");
 		String mobile = request.getParameter("mobile");
 		String email = request.getParameter("email");
 		String id = request.getParameter("id");
 
-		Connection connection = DriverManager.getConnection(url, username, password);
-		statement = connection.createStatement();
-
-		String query = "insert into user values ('" + name + "','" + mobile + "','" + email + "' , '" + id + "');";
-
-		statement.executeUpdate(query);
+		user.put("name", name);
+		user.put("mobile", mobile);
+		user.put("email", email);
+		user.put("userId", id);
 
 		return "Record Inserted";
 
@@ -48,46 +42,37 @@ public class Controller {
 	@PostMapping("/newOrder")
 	public String newOrder(HttpServletRequest request, HttpServletResponse res) throws SQLException {
 
+		order = new HashMap<>();
+
 		String quantity = request.getParameter("quantity");
 		String user = request.getParameter("user");
 		String item = request.getParameter("items");
 		String id = request.getParameter("id");
 
-		Connection connection = DriverManager.getConnection(url, username, password);
-		statement = connection.createStatement();
-
-		String query = "insert into orders values ('" + quantity + "','" + user + "','" + item + "' , '" + id + "');";
-
-		statement.executeUpdate(query);
+		order.put("quantity", quantity);
+		order.put("user", user);
+		order.put("item", item);
+		order.put("orderId", id);
 
 		return "Order Record Inserted";
 
 	}
 
 	@GetMapping("/all_order/{user_id}")
-	public ArrayList<HashMap<String, String>> allOrder(@PathVariable("user_id") String id) throws SQLException {
-
-		Connection connection = DriverManager.getConnection(url, username, password);
-		statement = connection.createStatement();
-
-		String query = "select name, items, quantity from user u,orders o where o.orderId = u.id";
-		ResultSet resultSet = statement.executeQuery(query);
-
-		ArrayList<HashMap<String, String>> list = new ArrayList<>();
+	public HashMap<String, String> allOrder(@PathVariable("user_id") String id) throws SQLException {
 
 		HashMap<String, String> map = new HashMap<>();
 
-		while (resultSet.next()) {
-			map = new HashMap<>();
-			map.put("name", resultSet.getString("name"));
-			map.put("item", resultSet.getString("items"));
-			map.put("quantity", resultSet.getString("quantity"));
+		if (user.get(id) == order.get(id)) {
 
-			list.add(map);
+			for (Map.Entry val : order.entrySet()) {
+
+				map.put("" + val.getKey(), "" + val.getValue());
+
+			}
 
 		}
-
-		return list;
+		return map;
 
 	}
 
@@ -95,13 +80,7 @@ public class Controller {
 	public String update(HttpServletRequest request, HttpServletResponse res, @PathVariable("item") String item)
 			throws SQLException {
 
-		Connection connection = DriverManager.getConnection(url, username, password);
-		statement = connection.createStatement();
-
 		String orderId = request.getParameter("orderId");
-
-		String query = "update orders set items = '" + item + "' where orderId = '" + orderId + "' ";
-		statement.executeUpdate(query);
 
 		return "Record Updated";
 
@@ -110,13 +89,7 @@ public class Controller {
 	@DeleteMapping("/deleteOrder")
 	public String delete(HttpServletRequest request, HttpServletResponse res) throws SQLException {
 
-		Connection connection = DriverManager.getConnection(url, username, password);
-		statement = connection.createStatement();
-
 		String orderId = request.getParameter("orderId");
-
-		String query = "delete from orders where orderId = '" + orderId + "' ";
-		statement.executeUpdate(query);
 
 		return "Record Deleted";
 
